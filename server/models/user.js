ed9579b082c87b84404c1ae50198300d8e590242
ahmed <ahmedhaza14@gmail.com>
2025-04-21 06:13:20 +0200

    Redeploy

--- server/models/user.js
+++ server/models/user.js
@@ -3,9 +3,9 @@ import mongoose, { mongo } from 'mongoose'
 const userSchema =new mongoose.Schema(
     {
         _id:{type:String ,required:true},
-        name: {type:string ,required:true},
-        email: {type:string ,required:true},
-        imageUrl: {type:string ,required:true},
+        name: {type:String ,required:true},
+        email: {type:String ,required:true},
+        imageUrl: {type:String ,required:true},
         enrolledCourses:[
             {
                 type:mongoose.Schema.Types.ObjectId,
